c24ae156a6f1bdfc3e8029f58131c8bd729df087
heejung1180 <heejung1180@outlook.com>
2024-05-17 18:26:50 -0700

    async function to update crew page with engineer info

--- script.js
+++ script.js
@@ -11,19 +11,17 @@ console.log(dataString) // {"name":"Moon"}
 
 // fetch is used to make a GET request
 // .json() method parses JSON responses into usable JavaScript object literal or array automatically
-fetch('https://raw.githubusercontent.com/heejung-hong/space-tourism-website/main/data.json')
-  .then(res => res.json())
-  .then(data => {
-    console.log(data)
-    let role = data.crew[0].role.toUpperCase();
-    document.querySelector('#role').textContent = role;
-    let name = data.crew[0].name.toUpperCase();;
-    document.querySelector('#name').textContent = name;
-    let bio = data.crew[0].bio;
-    document.querySelector('#bio').textContent = bio;
-    let image = data.crew[0].images.png;
-    document.querySelector('#image').src = image;
-    document.querySelector('#image').alt = "commander";
-  });
-
-
+async function engineer() {
+  const response = await fetch('https://raw.githubusercontent.com/heejung-hong/space-tourism-website/main/data.json')
+  const data = await response.json();
+  console.log(data);
+  let role = data.crew[3].role.toUpperCase();
+  document.querySelector('#role').textContent = role;
+  let name = data.crew[3].name.toUpperCase();;
+  document.querySelector('#name').textContent = name;
+  let bio = data.crew[3].bio;
+  document.querySelector('#bio').textContent = bio;
+  let image = data.crew[3].images.png;
+  document.querySelector('#image').src = image;
+  document.querySelector('#image').alt = "commander";
+}
