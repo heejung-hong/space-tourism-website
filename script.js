8c33f7ba2f976cad2b981e4b468ba30c00cd9b79
heejung1180 <heejung1180@outlook.com>
2024-05-17 18:40:48 -0700

    function to update crew page when clicking on dots

--- script.js
+++ script.js
@@ -11,10 +11,65 @@ console.log(dataString) // {"name":"Moon"}
 
 // fetch is used to make a GET request
 // .json() method parses JSON responses into usable JavaScript object literal or array automatically
+fetch('https://raw.githubusercontent.com/heejung-hong/space-tourism-website/main/data.json')
+  .then(response => response.json())
+  .then(data => {
+    let role = data.crew[0].role.toUpperCase();
+    document.querySelector('#role').textContent = role;
+    let name = data.crew[0].name.toUpperCase();;
+    document.querySelector('#name').textContent = name;
+    let bio = data.crew[0].bio;
+    document.querySelector('#bio').textContent = bio;
+    let image = data.crew[0].images.png;
+    document.querySelector('#image').src = image;
+    document.querySelector('#image').alt = "commander";
+  })
+  
+async function commander() {
+  const response = await fetch('https://raw.githubusercontent.com/heejung-hong/space-tourism-website/main/data.json')
+  const data = await response.json();
+  let role = data.crew[0].role.toUpperCase();
+  document.querySelector('#role').textContent = role;
+  let name = data.crew[0].name.toUpperCase();;
+  document.querySelector('#name').textContent = name;
+  let bio = data.crew[0].bio;
+  document.querySelector('#bio').textContent = bio;
+  let image = data.crew[0].images.png;
+  document.querySelector('#image').src = image;
+  document.querySelector('#image').alt = "commander";
+}
+
+async function specialist() {
+  const response = await fetch('https://raw.githubusercontent.com/heejung-hong/space-tourism-website/main/data.json')
+  const data = await response.json();
+  let role = data.crew[1].role.toUpperCase();
+  document.querySelector('#role').textContent = role;
+  let name = data.crew[1].name.toUpperCase();;
+  document.querySelector('#name').textContent = name;
+  let bio = data.crew[1].bio;
+  document.querySelector('#bio').textContent = bio;
+  let image = data.crew[1].images.png;
+  document.querySelector('#image').src = image;
+  document.querySelector('#image').alt = "mission specialist";
+}
+
+async function pilot() {
+  const response = await fetch('https://raw.githubusercontent.com/heejung-hong/space-tourism-website/main/data.json')
+  const data = await response.json();
+  let role = data.crew[2].role.toUpperCase();
+  document.querySelector('#role').textContent = role;
+  let name = data.crew[2].name.toUpperCase();;
+  document.querySelector('#name').textContent = name;
+  let bio = data.crew[2].bio;
+  document.querySelector('#bio').textContent = bio;
+  let image = data.crew[2].images.png;
+  document.querySelector('#image').src = image;
+  document.querySelector('#image').alt = "pilot";
+}
+
 async function engineer() {
   const response = await fetch('https://raw.githubusercontent.com/heejung-hong/space-tourism-website/main/data.json')
   const data = await response.json();
-  console.log(data);
   let role = data.crew[3].role.toUpperCase();
   document.querySelector('#role').textContent = role;
   let name = data.crew[3].name.toUpperCase();;
@@ -23,5 +78,5 @@ async function engineer() {
   document.querySelector('#bio').textContent = bio;
   let image = data.crew[3].images.png;
   document.querySelector('#image').src = image;
-  document.querySelector('#image').alt = "commander";
+  document.querySelector('#image').alt = "flight engineer";
 }
